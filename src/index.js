1b8a7f8e6853e090decb79d41f4ad597dbb1f940
Kristoffer Karlsson <kristoffer.karlsson@isotop.se>
2017-09-02 21:25:26 +0200

    Cleaned up so we just publish the component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Component } from 'react';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+class Offline extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			offline: false
+		};
+	}
+
+	componentDidMount() {
+		setInterval(() => {
+  			var xhr = new XMLHttpRequest();
+
+  			xhr.onreadystatechange = () => {
+  				this.setState({ offline: xhr.status !== 200 && xhr.status !== 404 });
+  			}
+
+  			xhr.open('GET', 'favicon.ico');
+  			xhr.setRequestHeader('Cache-Control', 'no-cache');
+  			xhr.send();
+  		}, this.props.interval || 5000);
+	}
+
+	render() {
+		const { children, offlineComponent: OfflineComponent } = this.props;
+		return this.state.offline ? <OfflineComponent /> : (
+			<div>
+				{this.props.children}
+			</div>
+		);
+	}
+}
+
+export default Offline;
